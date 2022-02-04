2288321f478558bbe9530dc4a6ea550672a2bfb2
Mateus Peternelli <mateuspeternelli@gmail.com>
2022-02-04 10:11:58 -0300

    Cração da classe C

---
+++ C.java
@@ -0,0 +1,12 @@
+public class C{
+    String C1;
+    int C2;
+
+    public void MC1(){
+        System.out.print("MC1");
+    }
+
+    public void MC2(){
+        System.out.print("MC2");
+    }
+}
